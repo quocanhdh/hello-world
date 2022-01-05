ec184d37844aa2a139758b92844647d9f35f712b
Anhtqhe163835 <90888573+quocanhdh@users.noreply.github.com>
2022-01-05 09:17:37 +0700

    Add files via upload

---
+++ Zoom/lesson_2/lesson_1/main.js
@@ -0,0 +1,12 @@
+var fullName = 'dcmmm';
+
+var firstName = 'Quoc'
+
+var lastName = 'Anh'
+console.log(firstName + " " + lastName);
+document.write('hello');
+
+
+var arr = [1,2,3,4,5,6];
+console.log(arr[3]);
+
